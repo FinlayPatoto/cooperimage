1fda6a9f448e2eb04807c4be4e2f72fc9402bd3b
Finlay Patoto <100161182+FinlayPatoto@users.noreply.github.com>
2022-11-12 18:31:08 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/logo.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Hosted by <img src="/logo.svg" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
